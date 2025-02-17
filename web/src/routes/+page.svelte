f940ac8dba22b25dd544f2ed024066f02cacf008
Bob Fanger <bob.fanger@this.nl>
2025-02-17 08:52:15 +0100

    feat: Add link to  downloads

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -11,12 +11,32 @@
 
 <div class="centered">
   <Button href="/app">Continue with web</Button>
+
+  <a class="link" href="https://github.com/bfanger/pr-tool/releases">
+    Download the desktop app&hellip;
+  </a>
 </div>
 <slot />
 
 <style>
   .centered {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    align-items: center;
+
     padding-top: 4rem;
-    text-align: center;
+  }
+
+  .link {
+    font:
+      500 1.4rem "SF Pro Display",
+      var(--font);
+    color: #2b67d4;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
   }
 </style>
